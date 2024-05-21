5527ef8b1209b86f06c25ea12435ef2eb2d986ea
ttang <ttang@ttang-MacBook-Pro-2021.local>
2024-05-21 14:38:32 +0800

    feat: tmp no baseUrl

--- app/atttoken/frames/frames.ts
+++ app/atttoken/frames/frames.ts
@@ -2,5 +2,5 @@ import { createFrames } from "frames.js/next";
 
 export const frames = createFrames({
   basePath: "/atttoken/frames",
-  baseUrl: process.env.FRAMES_BASE_URL,
+  // baseUrl: process.env.FRAMES_BASE_URL,
 });
