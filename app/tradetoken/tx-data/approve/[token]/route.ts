f9d1dfd5dd8bba8b8b581f98afb9dd3f7695bffe
ttang <ttang@ttang-MacBook-Pro-2021.local>
2024-05-27 18:25:27 +0800

    feat: update framesjs

--- app/tradetoken/tx-data/approve/[token]/route.ts
+++ app/tradetoken/tx-data/approve/[token]/route.ts
@@ -1,77 +1,67 @@
 import { API_KEY_0X_API_KEY } from "@/lib/env";
-import { TransactionTargetResponse } from "frames.js";
-import { getFrameMessage } from "frames.js/next/server";
 import { NextRequest, NextResponse } from "next/server";
 import { parseEther, erc20Abi, encodeFunctionData } from "viem";
 import { base } from "viem/chains";
+import { transaction } from "frames.js/core";
+import { frames } from "../../../frames/frames";
 
 export async function POST(
   req: NextRequest,
   { params }: { params: { token: string } }
-): Promise<
-  NextResponse<
-    | TransactionTargetResponse
-    | {
-        errors: { reason: string; message: string }[];
-      }
-  >
-> {
+) {
   const { token } = params;
-  const { searchParams } = new URL(req.url);
-  const tokenAddress = searchParams.get("tokenAddress") || "";
-  const json = await req.json();
-  const frameMessage = await getFrameMessage(json);
 
-  if (!frameMessage) {
-    throw new Error("No frame message");
-  }
-  let amount = frameMessage.inputText || "";
-  if (!tokenAddress || !amount) {
-    return NextResponse.json(
-      {
-        errors: [
-          {
-            reason: "Invalid",
-            message: "Invalid request",
-          },
-        ],
-      },
-      {
-        status: 400,
-      }
-    );
-  }
+  return await frames(async (ctx) => {
+    const { message } = ctx;
+    const tokenAddress = ctx.searchParams.tokenAddress || "";
+    let amount = message?.inputText || "";
+    if (!tokenAddress || !amount) {
+      return NextResponse.json(
+        {
+          errors: [
+            {
+              reason: "Invalid",
+              message: "Invalid request",
+            },
+          ],
+        },
+        {
+          status: 400,
+        }
+      );
+    }
 
-  console.log("approve token", token, amount, tokenAddress);
-  const baseUrl = `https://base.api.0x.org/swap/v1/quote?`;
-  const eth = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+    console.log("approve token", token, amount, tokenAddress);
+    const baseUrl = `https://base.api.0x.org/swap/v1/quote?`;
+    const eth = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 
-  const querys = new URLSearchParams({
-    buyToken: eth, // address
-    sellToken: tokenAddress,
-    sellAmount: parseEther(amount).toString(),
-  }).toString();
+    const querys = new URLSearchParams({
+      buyToken: eth, // address
+      sellToken: tokenAddress,
+      sellAmount: parseEther(amount).toString(),
+    }).toString();
 
-  const res = await fetch(baseUrl + querys, {
-    headers: { "0x-api-key": API_KEY_0X_API_KEY! },
-  });
+    const res = await fetch(baseUrl + querys, {
+      headers: { "0x-api-key": API_KEY_0X_API_KEY! },
+    });
 
-  const order = await res.json();
+    const order = await res.json();
 
-  const calldata = encodeFunctionData({
-    abi: erc20Abi,
-    functionName: "approve",
-    args: [order.to, parseEther(amount)],
-  });
-  console.log("calldata", calldata);
-
-  return NextResponse.json({
-    chainId: `eip155:${base.id}`,
-    method: "eth_sendTransaction",
-    params: {
+    const calldata = encodeFunctionData({
       abi: erc20Abi,
-      to: tokenAddress as `0x${string}`,
-      data: calldata,
-    },
-  });
+      functionName: "approve",
+      args: [order.to, parseEther(amount)],
+    });
+    console.log("calldata", calldata);
+
+    return transaction({
+      chainId: `eip155:${base.id}`,
+      method: "eth_sendTransaction",
+      params: {
+        abi: erc20Abi,
+        to: tokenAddress as `0x${string}`,
+        data: calldata,
+      },
+    });
+  })(req);
 }
