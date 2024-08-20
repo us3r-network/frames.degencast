18f8047143918ac3a2d2fc053929acb409a0516f
shixuewen <friendlysxw@163.com>
2024-08-20 12:14:27 +0800

    feat: Add the cast action of vote

--- lib/createproposal/neynar-api.ts
+++ lib/createproposal/neynar-api.ts
@@ -15,3 +15,22 @@ export const getCastWithHash = async (hash: string) => {
   const data = await request.json();
   return data?.cast as NeynarCast;
 };
+
+export const neynarValidateFrameMessage = async (messageBytes: any) => {
+  const url = "https://api.neynar.com/v2/farcaster/frame/validate";
+  const options = {
+    method: "POST",
+    headers: NeynarApiHeaders,
+    body: JSON.stringify({
+      cast_reaction_context: false,
+      follow_context: false,
+      signer_context: false,
+      channel_follow_context: false,
+      message_bytes_in_hex: messageBytes,
+    }),
+  };
+
+  const request = await fetch(url, options as any);
+  const data = await request.json();
+  return data;
+};
