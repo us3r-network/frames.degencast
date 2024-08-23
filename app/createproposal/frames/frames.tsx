c856bd70e4e580d78231406abdf92dc2fc58f4d6
shixuewen <friendlysxw@163.com>
2024-08-23 17:11:28 +0800

    feat: imagesWorker

--- app/createproposal/frames/frames.tsx
+++ app/createproposal/frames/frames.tsx
@@ -1,9 +1,5 @@
+import { imagesWorkerMiddleware } from "frames.js/middleware/images-worker";
 import { createFrames } from "frames.js/next";
-
-export const frames = createFrames({
-  basePath: "/createproposal",
-  baseUrl: process.env.FRAMES_BASE_URL,
-});
 
 import * as fs from "node:fs";
 import * as path from "node:path";
@@ -17,6 +13,17 @@ const InterFontMedium = fs.readFileSync(
 const InterBoldFont = fs.readFileSync(
   path.join(process.cwd(), "public/fonts/inter/Inter-Bold.otf")
 );
+
+export const frames = createFrames({
+  basePath: "/createproposal",
+  baseUrl: process.env.FRAMES_BASE_URL,
+  middleware: [
+    imagesWorkerMiddleware({
+      imagesRoute: "/frames/images",
+      secret: "degencast",
+    }),
+  ],
+});
 
 export const imageOptions: {
   aspectRatio?: "1.91:1" | "1:1";
