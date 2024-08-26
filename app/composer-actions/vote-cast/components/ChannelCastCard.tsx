212178579a645336b6891d6939b71e74582e5d98
shixuewen <friendlysxw@163.com>
2024-08-26 19:10:46 +0800

    feat: composer action

---
+++ app/composer-actions/vote-cast/components/ChannelCastCard.tsx
@@ -0,0 +1,46 @@
+import { NeynarCast } from "@/lib/createproposal/neynar-types";
+import { AttentionTokenEntity } from "@/lib/createproposal/types/attention-token";
+import { CommunityEntity } from "@/lib/createproposal/types/community";
+import { ProposalEntity } from "@/lib/createproposal/types/proposal";
+import { cn } from "@/lib/utils";
+import React from "react";
+import { ProposalStatus } from "./ProposalStatus";
+import { ProposalButton } from "./ProposalButton";
+import { getCastImageUrl } from "@/lib/cast";
+import { Card } from "@/components/ui/card";
+
+type ChannelCastCardProps = {
+  channel: CommunityEntity;
+  tokenInfo: AttentionTokenEntity;
+  cast: NeynarCast;
+  proposal: ProposalEntity;
+};
+const ChannelCastCard = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & ChannelCastCardProps
+>(({ channel, proposal, cast, tokenInfo, className }, ref) => {
+  const { channelId } = channel || {};
+  return (
+    <Card
+      className={cn(
+        "box-border w-full flex-col gap-4 rounded-[20px] border-none p-4",
+        className
+      )}
+      ref={ref}
+    >
+      <div>
+        <img src={getCastImageUrl(cast.hash)} alt="" />
+      </div>
+      <div className="flex flex-row items-center justify-between mt-4">
+        <div>
+          <ProposalStatus proposal={proposal} />
+        </div>
+        <div>
+          <ProposalButton proposal={proposal} cast={cast} />
+        </div>
+      </div>
+    </Card>
+  );
+});
+
+export default ChannelCastCard;
---
+++ app/composer-actions/vote-cast/components/ProposalButton.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { getWarpcastConversationUrl } from "@/lib/cast";
+import { NeynarCast } from "@/lib/createproposal/neynar-types";
+import { ProposalEntity } from "@/lib/createproposal/types/proposal";
+import getFrameUrlWithState from "@/lib/proposal/getFrameUrlWithState";
+import { useMemo } from "react";
+
+export type ProposalButtonProps = {
+  cast: NeynarCast;
+  proposal: ProposalEntity;
+};
+export function ProposalButton({ proposal, cast }: ProposalButtonProps) {
+  const { status, roundIndex } = proposal;
+  const frameUrl = useMemo(() => {
+    return getFrameUrlWithState(status, cast.hash);
+  }, [status, roundIndex, cast]);
+  const castUrl = getWarpcastConversationUrl(cast.hash);
+
+  if (!frameUrl) {
+    return null;
+  }
+  return (
+    <Button
+      className="h-[32px] bg-[#9151C3] py-[6px] px-[8px]"
+      onClick={(e) => {
+        e.preventDefault();
+        window.parent.postMessage(
+          {
+            type: "createCast",
+            data: {
+              cast: {
+                text: "Use frame to vote the proposal",
+                embeds: [frameUrl, castUrl],
+              },
+            },
+          },
+          "*"
+        );
+      }}
+    >
+      <span className="text-xs">Pick</span>
+    </Button>
+  );
+}
