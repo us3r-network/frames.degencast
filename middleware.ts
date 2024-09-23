2e4936acbe001ca5bf9b9b38832f2c9465739777
ttang <ttang@ttang-MBP-2021.lan>
2024-09-23 12:21:20 +0800

    feat: fix middleware

--- middleware.ts
+++ middleware.ts
@@ -3,35 +3,35 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-  // if (isBrowser(req)) {
-  //   if (
-  //     pathname === "/curationchannel/frames/channels" ||
-  //     pathname === "/waitlist/frames"
-  //   ) {
-  //     return NextResponse.redirect(new URL(`${DEGENCAST_WEB_URL}`));
-  //   }
-  //   if (
-  //     pathname.startsWith("/proposal/frames") ||
-  //     pathname.startsWith("/curationnft/frames")
-  //   ) {
-  //     const castHash = req.nextUrl.searchParams.get("castHash") || "";
-  //     return NextResponse.redirect(
-  //       new URL(`${DEGENCAST_WEB_URL}/casts/${castHash}`)
-  //     );
-  //   }
-  //   if (pathname.startsWith("/createproposal/frames")) {
-  //     const castHash = req.nextUrl.pathname.split("/").pop();
-  //     return NextResponse.redirect(
-  //       new URL(`${DEGENCAST_WEB_URL}/casts/${castHash}`)
-  //     );
-  //   }
-  //   if (pathname.startsWith("/curationchannel/frames")) {
-  //     const channel = req.nextUrl.searchParams.get("channelId") || "home";
-  //     return NextResponse.redirect(
-  //       new URL(`${DEGENCAST_WEB_URL}/communities/${channel}`)
-  //     );
-  //   }
-  // }
+  if (isBrowser(req)) {
+    if (
+      pathname === "/curationchannel/frames/channels" ||
+      pathname === "/waitlist/frames"
+    ) {
+      return NextResponse.redirect(new URL(`${DEGENCAST_WEB_URL}`));
+    }
+    if (
+      pathname.startsWith("/proposal/frames") ||
+      pathname.startsWith("/curationnft/frames")
+    ) {
+      const castHash = req.nextUrl.searchParams.get("castHash") || "";
+      return NextResponse.redirect(
+        new URL(`${DEGENCAST_WEB_URL}/casts/${castHash}`)
+      );
+    }
+    if (pathname.startsWith("/createproposal/frames")) {
+      const castHash = req.nextUrl.pathname.split("/").pop();
+      return NextResponse.redirect(
+        new URL(`${DEGENCAST_WEB_URL}/casts/${castHash}`)
+      );
+    }
+    if (pathname.startsWith("/curationchannel/frames")) {
+      const channel = req.nextUrl.searchParams.get("channelId") || "home";
+      return NextResponse.redirect(
+        new URL(`${DEGENCAST_WEB_URL}/communities/${channel}`)
+      );
+    }
+  }
 
   return NextResponse.next();
 }
