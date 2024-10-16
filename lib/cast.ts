352eb36cd4713fd39c1d3a4e5d5c999bcba6a978
shixuewen <friendlysxw@163.com>
2024-10-16 10:27:34 +0800

    fix: warpcast image url

--- lib/cast.ts
+++ lib/cast.ts
@@ -4,6 +4,9 @@ export const getCastImageUrl = (castHash: string) => {
   return `${DEGENCAST_API}/3r-farcaster/cast-image?castHash=${castHash}`;
 };
 
+export const getWarpcastImageUrl = (castHash: string) => {
+  return `https://client.warpcast.com/v2/cast-image?castHash=${castHash}`;
+};
 export const getWarpcastConversationUrl = (castHash: string) => {
   return `https://warpcast.com/~/conversations/${castHash}`;
 };
